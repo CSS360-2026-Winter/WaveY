65a641897a0b82a8d169df04b5409c6cb0d77270
Jayda Fountain <140588628+moonflowerrr@users.noreply.github.com>
2026-02-28 03:54:23 +0000

    Remove unused memebot code.

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -5,33 +5,4 @@ export default {
     if (message.author.bot) return;
     },
 };
-    /*
-    //Old code: 
-    //const state = activeTrivia.get(message.author.id);
-    //if (!state) return;
-
-    const userId = message.author.id;
-    const session = activeTrivia.get(userId);
-    if(!session)return; 
-
-
-    const userAnswer = message.content.trim().toLowerCase();
-    if (!["a", "b", "c", "d"].includes(userAnswer)) return;
-
-    const { message: response, correct } = evaluateAnswer(
-      userAnswer,
-      session.correctAnswer
-    );
-
-    await message.reply(response);
-
-    if(correct) session.score += 1; 
-
-    session.questionCount += 1;
-
-    activeTrivia.set(userId, session);
-
-    //get rid of delete since implementing question count/exit command
-    //activeTrivia.delete(message.author.id);
-  */
   
